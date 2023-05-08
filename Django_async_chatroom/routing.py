b72f5a7c0d5238fa011ba5a8457803801b5a2917
echewisi <josephechewisi@gmail.com>
2023-05-08 01:32:56 +0100

    added routes

--- Django_async_chatroom/routing.py
+++ Django_async_chatroom/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, UrlRouter
 import chatroom.routing
 
-application= ProtocolTypeRouter({    'websocket': AuthMiddlewareStack(
+application= ProtocolTypeRouter({ 'websocket': AuthMiddlewareStack(
         UrlRouter(
             chatroom.routing.websocket_urlpatterns
         ),
